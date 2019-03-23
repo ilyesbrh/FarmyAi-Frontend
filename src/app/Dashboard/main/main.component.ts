d56460ee52421e58e521a49190d4b61ad22db1a5
ilies bourouh <ilies.bourouh@univ-bba.dz>
2019-03-23 15:08:16 +0100

    swal popup Notification interval (half DONE)

--- src/app/Dashboard/main/main.component.ts
+++ src/app/Dashboard/main/main.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ChangeDetectorRef, OnDestroy, ViewChild } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { SwalPartialTargets, SwalComponent } from '@toverux/ngx-sweetalert2';
-import { State,getDetections,getUnseen } from '../../Store/index';
+import { State, getDetections, getUnseen } from '../../Store/index';
 import { Store } from '@ngrx/store';
 import { detection } from 'src/app/Store/model';
 import { Observable } from 'rxjs/internal/Observable';
 import { ApiInterfaceService } from 'src/app/api-interface.service';
-
+import { interval } from 'rxjs';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-main',
@@ -19,7 +20,7 @@ export class MainComponent implements OnInit, OnDestroy {
 
   mobileQuery: MediaQueryList;
 
-  popup : {animal:string,accuracy:number}= {animal:"",accuracy:-1};
+  popup: { animal: string, accuracy: number } = { animal: "", accuracy: -1 };
   public profileURL = "https://api.adorable.io/avatars/110/iliesbrh";
 
   private _mobileQueryListener: () => void;
@@ -27,8 +28,8 @@ export class MainComponent implements OnInit, OnDestroy {
   Undiscovered: Observable<detection[]>;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
-     public readonly swalTargets: SwalPartialTargets,public store:Store<State>
-     ,public api:ApiInterfaceService) {
+    public readonly swalTargets: SwalPartialTargets, public store: Store<State>
+    , public api: ApiInterfaceService) {
     this.mobileQuery = media.matchMedia('(max-width: 750px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -40,35 +41,28 @@ export class MainComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    var UneNotif = 1;
-     setInterval(() => {
-       this.api.getLiveUpdate().subscribe((val)=>{
-        if(val.length>0 && UneNotif===1){
-          this.popup.animal = val[0].class;
-          this.popup.accuracy = val[0].confidence;
+
+    interval(60 * 1000).subscribe(data => {
+      this.api.getLiveUpdate().subscribe(val =>{
+        if(val[0]){
+          this.SwalPopup.title = val.class+' Detected';
+          this.SwalPopup.text = 'accuracy '+val.confidence+'%';
           this.SwalPopup.show();
-          UneNotif=0;
         }
-       });
-    }, 10000); 
+      })
+    });
 
 
-    
     this.Notifications = this.store.select<detection[]>(getDetections);
     this.Undiscovered = this.store.select<detection[]>(getUnseen);
-    this.store.select<detection[]>(getUnseen).subscribe((val)=>{
-      console.log(val);
-    });
-    
 
-  
   }
-  
+
   ngAfterViewInit(): void {
   }
-  goToHistory(event:any){
+  goToHistory(event: any) {
     console.log(event);
-    
+
   }
 
 }
